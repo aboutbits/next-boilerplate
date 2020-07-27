1e5585c69531010ba1582fff0d863e77036ded6b
Martin Malfertheiner <martin.malfertheiner@aboutbits.it>
2020-07-27 15:24:38 +0200

    add some routes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Link from 'next/link'
 
 const Home: React.FC = () => {
   return (
@@ -9,7 +10,32 @@ const Home: React.FC = () => {
       </Head>
 
       <main>
-        <h1 className="text-lg font-bold text-center">AboutBits Next.js Boilerplate</h1>
+        <h1 className="text-lg font-bold">AboutBits Next.js Boilerplate</h1>
+
+        <ul className="space-y-4 mx-10 mt-10">
+          <li>
+            <Link href="/projects/[id]" as="/projects/1">
+              <a>
+                Project 1
+              </a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/projects/[id]" as="/projects/2">
+              <a>
+                Project 2
+              </a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/projects/[id]" as="/projects/3">
+              <a>
+                Project 3
+              </a>
+            </Link>
+          </li>
+        </ul>
+
       </main>
     </div>
   )
